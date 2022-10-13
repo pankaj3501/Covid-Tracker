daa1bc9921fa2a6fa72c8a57b63bee22f59a7348
pankaj3501 <mr.pankajkumar3501@gmail.com>
2022-10-13 19:08:10 +0530

    add patientRegistration

--- backend(covid-traker)/src/main/java/com/java/demo/PatientRegistration.java
+++ backend(covid-traker)/src/main/java/com/java/demo/PatientRegistration.java
@@ -2,8 +2,23 @@ package com.java.demo;
 
 public class PatientRegistration {
 	
+	private int id ;
+	private String name ;
+	private boolean gender;
+	private date birthday;
+	private String mobile;
+	private String email;
+	private boolean country;
+	private boolean city;
+	private boolean state;
+	private String block;
+	private date testingdate;
+	private String report;
+	private String patientStatus;
+	private String suggestion;
+	private button registration;
 	
-	// helloo java to backend connect
+	
 	
 
 }
